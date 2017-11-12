e7aafe9b5e386491324210aeff33b9f542d7c5a7
Thatalent <jonathan.hudson2012@gmail.com>
2017-11-12 18:51:44 -0500

    Made inital changes for battle wave code.

---
+++ TurnbasedProject/Assets/Scripts/TurnBaseSystem/BattleSystem/BattleBuilder/BattleFactoryService.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class BattleFactoryService : BattleFactory {
+
+    /// <summary>
+    /// Returns a new instance of an implement child class for Battle Factory based on the player's location in the game universe.
+    /// </summary>
+    /// <param name="location"></param>
+    /// <returns></returns>
+	static public BattleFactory getNewBattleFactory(PlayerLocation location)
+    {
+
+    }
+}
