e3a71970844a7392e14bf3fac9661a02004ea6d2
Jonathan Hudson <jonathan.hudson2012@gmail.com>
2018-11-05 10:48:36 -0500

    Update CharacterCreation.cs

--- Absolute-Horizon.git/branches/master/TurnbasedProject/Assets/Scripts/Character/CharacterCreation.cs
+++ Absolute-Horizon.git/branches/master/TurnbasedProject/Assets/Scripts/Character/CharacterCreation.cs
@@ -1,52 +1,52 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.SceneManagement;
-using System.Collections;
+using System.Collections.Generic;
 
-public class CharacterCreation : MonoBehaviour {
+public class CharacterCreation : MonoBehaviour
+{
 
-	private Player newPlayer;
-	private string playerName;
-	private bool isStudent;
-	private bool isRoyal;
+    private Player newPlayer;
+    private string playerName;
+    private bool isStudent;
+    private bool isRoyal;
 
 
 
-	// Use this for initialization
-	void Start () {
-		NewPlayer = new Player ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-	void OnGUI(){
+    // Use this for initialization.
+    void Start()
+    {
+        NewPlayer = new Player();
+    }
 
-		isStudent = GUILayout.Toggle (isStudent, "Student Class");
-		isRoyal = GUILayout.Toggle (isRoyal, "Royal Class");
-		if(GUILayout.Button ("Create")){
-			if(isStudent){
-				NewPlayer.CharacterClass=new StudentCharacterClass();
-			}
-			else if(isRoyal){
-				NewPlayer.CharacterClass=new RoyalCharacterClass();
-			}
-			NewPlayer.PlayerLvl=1;
-			NewPlayer.Stamina = NewPlayer.CharacterClass.Stamina;
-			Debug.Log (NewPlayer.Stamina);
-			NewPlayer.Strength= NewPlayer.CharacterClass.Strength;
-			NewPlayer.Endurance=NewPlayer.CharacterClass.Endurance;
-			NewPlayer.Intelligence=NewPlayer.CharacterClass.Intelligence;
-			NewPlayer.Resistance=NewPlayer.CharacterClass.Resistance;
-			NewPlayer.Skill=NewPlayer.CharacterClass.Skill;
-			NewPlayer.Speed=NewPlayer.CharacterClass.Speed;
-			NewPlayer.Luck =NewPlayer.CharacterClass.Luck;
-			NewPlayer.PlayerExp=0f;
-			GameInformtion.save (NewPlayer);
-			BattleController.Player=GameInformtion.data();
-			Debug.Log (BattleController.Player.Skill);
-			MovesAssignment.attackMove ("Stab");
-            MovesAssignment.magicMove("Sparkles");
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnGUI()
+    {
+
+        isStudent = GUILayout.Toggle(isStudent, "Student Class");
+        isRoyal = GUILayout.Toggle(isRoyal, "Royal Class");
+		BaseCharacterClass playerClass = null;
+        if (GUILayout.Button("Create"))
+        {
+            if (isStudent)
+            {
+				playerClass = new StudentCharacterClass();
+            }
+            else if (isRoyal)
+            {
+				playerClass = new RoyalCharacterClass();
+            }
+
+			addClassToPlayer(playerClass);
+            GameInformation.Actions.isMenu5Locked = true;
+            GameInformation.Actions.isMenu3Locked = true;
+            GameInformation.Actions.isMenu4Locked = true;
+			MovesAssignment.assignMove("Stab");
+			MovesAssignment.assignMove("DualStrike");
+			MovesAssignment.assignMove("ShockWave");
             Debug.Log(GameInformtion.Spells[0]);
             Options.loadMoves();
 			SceneManager.LoadScene ("BattleScence");
